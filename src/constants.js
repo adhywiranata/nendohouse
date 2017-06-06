11b0c33cf3db2df3a0ccd8152f75d721b4b25716
Adhy <adhywiranatap@gmail.com>
2017-06-06 10:20:53 +0700

    add color constants

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const colors = {
+  blue: '#3498DB',
+  white: '#FFFFFF',
+  darkerWhite: '#F5F5F5',
+  red: '#C0392B',
+  green: '#26A65B',
+  orange: '#EB9532',
+  yellow: '#F5AB35',
+  black: '#000000',
+  grey: '#353535',
+};
