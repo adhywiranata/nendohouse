1be5146b8500ac534eee23dd969c40d82c243e1e
Adhy <adhywiranatap@gmail.com>
2017-06-13 13:42:26 +0700

    update 404 page

--- src/containers/Page404.js
+++ src/containers/Page404.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import WideSection from '../components/core/Layout/WideSection';
 
-export default class Page404 extends React.Component {
-  render() {
-    return (
-      <div style={{ paddingTop: 60 }}>
-        <WideSection>
-          <h1>404</h1>
-        </WideSection>
-      </div>
-    );
-  }
-}
+export default () => (
+  <div style={{ paddingTop: 60 }}>
+    <WideSection>
+      <h1>404</h1>
+    </WideSection>
+  </div>
+);
