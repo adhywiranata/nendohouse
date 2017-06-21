e3a224c1223ba1f71af2744b51e9031ab4c2526c
Adhy <adhywiranatap@gmail.com>
2017-06-21 07:01:06 +0700

    add firebase integration and sample data update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { Provider } from 'react-redux';
+import firebase from 'firebase';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { LazyComponent, lazify } from 'react-code-split-component';
 
@@ -15,19 +16,50 @@ const Heads = () => (
   </Helmet>
 );
 
-export default () => (
-  <Provider store={store}>
-    <BrowserRouter>
-      <div>
-        <Heads />
-        <Header />
-        <Switch>
-          <Route exact path="/" component={() => <LazyComponent load={() => import('./containers/HomePage')} />} />
-          <Route path="/search" component={() => <LazyComponent load={() => import('./containers/SearchResultPage')} />} />
-          <Route path="/:category/:title" component={() => <LazyComponent load={() => import('./containers/DetailPage')} />} />
-          <Route component={lazify(import('./containers/Page404'))} />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  </Provider>
-);
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: {},
+    };
+  }
+
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyD1W_bujrLv5GR3WAP9lh6K1x5JDliZENg',
+      authDomain: 'nendohouse.firebaseapp.com',
+      databaseURL: 'https://nendohouse.firebaseio.com',
+      projectId: 'nendohouse',
+      storageBucket: 'nendohouse.appspot.com',
+      messagingSenderId: '1028604632366',
+    };
+    firebase.initializeApp(config);
+  }
+
+  componentDidMount() {
+    const dat = firebase.database().ref('products').once('value').then((snapshot) => {
+      console.log(snapshot.val());
+    });
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+            <Heads />
+            <Header />
+            <Switch>
+              <Route exact path="/" component={() => <LazyComponent load={() => import('./containers/HomePage')} />} />
+              <Route path="/search" component={() => <LazyComponent load={() => import('./containers/SearchResultPage')} />} />
+              <Route path="/:category/:title" component={() => <LazyComponent load={() => import('./containers/DetailPage')} />} />
+              <Route component={lazify(import('./containers/Page404'))} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
+}
+
+export default App;
