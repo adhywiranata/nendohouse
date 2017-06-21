f376adc6bffe552cbb92934aef1ddd89f52c558f
Adhy <adhywiranatap@gmail.com>
2017-06-21 21:57:42 +0700

    update rootReducer to combine category reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import productReducer from './productReducer';
+import categoryReducer from './categoryReducer';
 
 export default combineReducers({
   products: productReducer,
+  categories: categoryReducer,
 });
