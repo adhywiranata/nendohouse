ba0684125aa0ff06ee1a732efda53a3a321446ec
Adhy <adhywiranatap@gmail.com>
2017-06-21 09:09:08 +0700

    update productsReducer

--- src/reducers/productReducer.js
+++ src/reducers/productReducer.js
@@ -5,7 +5,10 @@ const initialState = {
 };
 
 export default (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case 'FETCH_PRODUCTS_SUCCESS': return {...state, data: action.payload}
+    default: return state;
+  }
 };
 
 export const selectAllProducts = productsData => productsData.data;
