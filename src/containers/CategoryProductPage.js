98e6f457cdb8e1e4436af50e3d6759d319638834
Adhy <adhywiranatap@gmail.com>
2017-06-21 22:39:04 +0700

    add category product page

---
+++ src/containers/CategoryProductPage.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { selectAllProducts } from '../reducers/productReducer';
+import { fetchProducts, fetchCategories } from '../actions';
+
+import ProductList from '../components/products/List';
+import WideSection from '../components/core/Layout/WideSection';
+
+class HomePage extends React.Component {
+  componentDidMount() {
+    this.props.fetchProducts();
+    this.props.fetchCategories();
+  }
+
+  render() {
+    return (
+      <div style={{ paddingTop: 60 }}>
+        <WideSection>
+          <ProductList
+            products={this.props.products}
+            isProductsFetching={this.props.isProductsFetching}
+          />
+        </WideSection>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  products: selectAllProducts(state.products, state.categories),
+  isProductsFetching: state.products.isFetching,
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchProducts: () => dispatch(fetchProducts()),
+  fetchCategories: () => dispatch(fetchCategories()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
