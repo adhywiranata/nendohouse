d86a2fd638f783f73c4524480ffe96716f44d62d
Adhy <adhywiranatap@gmail.com>
2017-06-21 08:28:59 +0700

    add reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import productReducer from './productReducer';
+
+export default combineReducers({
+  products: productReducer,
+});
+
+export const selectAllProducts = productsData => productsData.data;
---
+++ src/reducers/productReducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+  data: [],
+  isFetching: false,
+  isError: false,
+};
+
+export default (state = initialState, action) => {
+  return state;
+};
