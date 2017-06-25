7d0f7a5793174a79e75f44080d97a08b65defa7b
Adhy <adhywiranatap@gmail.com>
2017-06-25 22:00:45 +0700

    update Header responsiveness

--- src/components/core/Header/index.js
+++ src/components/core/Header/index.js
@@ -7,29 +7,65 @@ import Logo from './Logo';
 import SearchBar from './SearchBar';
 import Navbar from './NavBar';
 
-export default ({
-  products,
-  searchKeyword,
-  isSearchFocus,
-  handleSearchFocus,
-  handleSearchInput,
-  handleSearchBlur,
-}) => (
-  <div>
-    <Headerbar>
-      <Link to="/" style={{ textDecoration: 'none' }}>
-        <Logo>NENDOHOUSE</Logo>
-      </Link>
-      <SearchBar
-        searchKeyword={searchKeyword}
-        isSearchFocus={isSearchFocus}
-        products={products}
-        handleSearchBlur={handleSearchBlur}
-        handleSearchInput={handleSearchInput}
-        handleSearchFocus={handleSearchFocus}
-      />
-      <Navbar />
-    </Headerbar>
-    { isSearchFocus ? <LazyComponent load={() => import('./ScreenFocusOverlay')} /> : null }
-  </div>
+const MainLogo = () => (
+  <Link to="/" style={{ textDecoration: 'none' }}>
+    <Logo>NENDOHOUSE</Logo>
+  </Link>
 );
+
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      windowWidth: window.innerWidth,
+    };
+    window.addEventListener('resize', () => {
+      this.setState({
+        windowWidth: window.innerWidth,
+      });
+    });
+  }
+
+  render() {
+    const {
+      products,
+      searchKeyword,
+      isSearchFocus,
+      handleSearchFocus,
+      handleSearchInput,
+      handleSearchBlur,
+    } = this.props;
+    const { windowWidth } = this.state;
+    return (
+      <div>
+        <Headerbar>
+          { windowWidth < 768 ? (
+            <MainLogo />
+          ) : (
+            <div style={{
+              display: 'flex',
+              flex: 1,
+              flexDirection: 'row',
+              justifyContent: 'space-around',
+            }}
+            >
+              <div style={{ paddingTop: 10 }}>
+                <MainLogo />
+              </div>
+              <SearchBar
+                searchKeyword={searchKeyword}
+                isSearchFocus={isSearchFocus}
+                products={products}
+                handleSearchBlur={handleSearchBlur}
+                handleSearchInput={handleSearchInput}
+                handleSearchFocus={handleSearchFocus}
+              />
+              <Navbar />
+            </div>
+          )}
+        </Headerbar>
+        { isSearchFocus ? <LazyComponent load={() => import('./ScreenFocusOverlay')} /> : null }
+      </div>
+    );
+  }
+}
