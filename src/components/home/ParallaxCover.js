1d530bd581737e14fade6dcb781bd8d599fa0a05
Adhy <adhywiranatap@gmail.com>
2017-06-25 20:36:02 +0700

    modularize parallax cover in homepage

---
+++ src/components/home/ParallaxCover.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import CoverImg from '../../images/cover.jpg';
+
+export default class ParallaxCover extends React.Component {
+  componentDidMount() {
+    let currentYScroll = 0;
+    const parallaxCover = document.getElementById('parallaxCover');
+    window.addEventListener('scroll', () => {
+      if(window.pageYOffset === 0) {
+        parallaxCover.style.backgroundPosition = '0% -50px';
+        parallaxCover.style.WebkitFilter = `grayscale(70%) blur(0px)`;
+        return true;
+      }
+      let currVal = Number(parallaxCover.style.backgroundPosition.split(' ')[1].replace('px', ''));
+      let increment = 3;
+      if (window.pageYOffset > currentYScroll) {
+        increment = -3;
+      }
+      currentYScroll = window.pageYOffset;
+      parallaxCover.style.backgroundPosition = `0% ${currVal + increment}px`;
+      parallaxCover.style.WebkitFilter = `grayscale(70%) blur(3px)`;
+      return true;
+    });
+  }
+
+  render() {
+    return (
+      <div style={{ position: 'relative', height: 300, overflow: 'hidden' }}>
+        <div
+          id="parallaxCover"
+          style={{
+            transition: '0.1s',
+            background: `url(${CoverImg})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: '0% -50px',
+            backgroundAttachment: 'fixed',
+            backgroundSize: 'contain',
+            WebkitFilter: 'grayscale(70%)',
+            MozFilter: 'grayscale(70%)',
+            OFilter: 'grayscale(70%)',
+            MsFilter: 'grayscale(70%)',
+            filter: 'grayscale(70%)',
+            position: 'absolute',
+            left: -55,
+            right: -55,
+            top: -15,
+            bottom: -15,
+          }}
+        />
+        <div style={{ position: 'absolute', zIndex: 1, backgroundColor: 'rgba(0,0,0,0.3)', width: '100%', height: '100%' }} />
+        <div style={{ position: 'absolute', zIndex: 2, bottom: -10, left: 100, color: '#FFFFFF', fontSize: '1.3em' }}>
+          <h1>NENDOHOUSE COLLECTIONS</h1>
+          <p style={{ marginTop: -20 }}>Your #1 Nendoroid Marketplace</p>
+        </div>
+      </div>
+    );
+  }
+}
--- src/containers/pages/HomePage.js
+++ src/containers/pages/HomePage.js
@@ -5,64 +5,19 @@ import { selectAllProducts } from '../../reducers/productReducer';
 import { selectAllCategories } from '../../reducers/categoryReducer';
 import { fetchProducts, fetchCategories } from '../../actions';
 
+import ParallaxCover from '../../components/home/ParallaxCover';
 import ProductList from '../../components/products/List';
 import WideSection from '../../components/core/Layout/WideSection';
-import CoverImg from '../../images/cover.jpg';
 
 class HomePage extends React.Component {
   componentDidMount() {
     this.props.fetchProducts();
-    let currentYScroll = 0;
-    const parallaxCover = document.getElementById('parallaxCover');
-    window.addEventListener('scroll', () => {
-      if(window.pageYOffset === 0) {
-        parallaxCover.style.backgroundPosition = '0% -50px';
-        parallaxCover.style.WebkitFilter = `grayscale(70%) blur(0px)`;
-        return true;
-      }
-      let currVal = Number(parallaxCover.style.backgroundPosition.split(' ')[1].replace('px', ''));
-      let increment = 3;
-      if (window.pageYOffset > currentYScroll) {
-        increment = -3;
-      }
-      currentYScroll = window.pageYOffset;
-      parallaxCover.style.backgroundPosition = `0% ${currVal + increment}px`;
-      parallaxCover.style.WebkitFilter = `grayscale(70%) blur(3px)`;
-      return true;
-    });
   }
 
   render() {
     return (
       <div style={{ paddingTop: 60 }}>
-        <div style={{ position: 'relative', height: 300, overflow: 'hidden' }}>
-          <div
-            id="parallaxCover"
-            style={{
-              transition: '0.1s',
-              background: `url(${CoverImg})`,
-              backgroundRepeat: 'no-repeat',
-              backgroundPosition: '0% -50px',
-              backgroundAttachment: 'fixed',
-              backgroundSize: 'contain',
-              WebkitFilter: 'grayscale(70%)',
-              MozFilter: 'grayscale(70%)',
-              OFilter: 'grayscale(70%)',
-              MsFilter: 'grayscale(70%)',
-              filter: 'grayscale(70%)',
-              position: 'absolute',
-              left: -55,
-              right: -55,
-              top: -15,
-              bottom: -15,
-            }}
-          />
-          <div style={{ position: 'absolute', zIndex: 1, backgroundColor: 'rgba(0,0,0,0.3)', width: '100%', height: '100%' }} />
-          <div style={{ position: 'absolute', zIndex: 2, bottom: -10, left: 100, color: '#FFFFFF', fontSize: '1.3em' }}>
-            <h1>NENDOHOUSE COLLECTIONS</h1>
-            <p style={{ marginTop: -20 }}>Your #1 Nendoroid Marketplace</p>
-          </div>
-        </div>
+        <ParallaxCover />
         <WideSection>
           <ProductList
             products={this.props.products}
